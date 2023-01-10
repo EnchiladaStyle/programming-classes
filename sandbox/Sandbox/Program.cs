d4a174dd72a8327c6fa2edd4b4708cc82e92c6a4
EnchiladaStyle <jon19073@byui.edu>
2023-01-09 17:11:38 -0700

    James bond assignment

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,6 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+    
+    int x = 5;
+
+    Console.WriteLine(x);
+
+    
+    
+    string string1 = "string";
+
+    Console.WriteLine($"this is a formatted {string1}");
+    
     }
 } 
-
