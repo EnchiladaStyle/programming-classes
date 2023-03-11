bf7c18f88e294bf96f06b911de067492542ba5f4
EnchiladaStyle <jon19073@byui.edu>
2023-03-11 00:25:24 -0700

    practiced with poly

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,74 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+
+    Square thisSquare = new Square();
+    Circle thisCircle = new Circle();
+    thisSquare.SetColor("red");
+    thisSquare.SetSide(6);
+    thisCircle.setRadius(7);
+    thisCircle.SetColor("blue");
+
+
+    List<Shape> shapes = new List<Shape>();
+    shapes.Add(thisCircle);
+    shapes.Add(thisSquare);
+
+    foreach (Shape s in shapes)
+    {
+        Console.WriteLine();
+        Console.WriteLine(s.getArea());
+        Console.WriteLine(s.GetColor());
+        Console.WriteLine();
+    }
+    }
+    
+}
+
+public abstract class Shape
+{
+    private string _color;
+
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public abstract double getArea();
+    
+}
+
+public class Square : Shape
+{
+    private double _side;
+
+    public void SetSide(double side)
+    {
+        _side = side;
+    }
+
+    public override double getArea()
+    {
+        return _side * _side;
     }
 }
+
+public class Circle : Shape
+{
+    private double _radius;
+
+    public void setRadius(double radius)
+    {
+        _radius = radius;
+    }
+
+    public override double getArea()
+    {
+        return (3.14*(_radius*_radius));
+    }
+} 
