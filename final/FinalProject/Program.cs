e0da1d3172189eda34c08f4a3c1d434c5a1d7ff8
EnchiladaStyle <jon19073@byui.edu>
2023-03-29 22:52:57 -0600

    Started the bank account project

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -6,4 +6,99 @@ class Program
     {
         Console.WriteLine("Hello FinalProject World!");
     }
+
+    public abstract class Account
+    {
+        private string _username;
+        private string _password;
+        private int _balance = 0;
+        private int _dept = 0;
+
+        public void setUsername(int username)
+        {
+            _username = username;
+        }
+
+        public void setPassword(int password)
+        {
+            _password = password;
+        }
+
+        public void displayBalance()
+        {
+            Console.Writeline(balance);
+        }
+
+        public virtual void displayDept()
+        {
+            Console.WriteLine(_dept);
+        }
+    }
+
+    public class Banker : Account
+    {
+        public void countAllFunds()
+        {
+
+        }
+    }
+
+    public class Customer : Account
+    {
+        public void writeReview()
+        {
+
+        }
+    }
+
+    public class Child : Account 
+    {
+        public override displayDept()
+        {
+            Console.WriteLine('You are not permitted to take out loans.');
+        }
+    }
+
+    public class Transaction
+    {
+        private int _originalBalance;
+        private int _modifiedBalance;
+    }
+
+    public class Deposit : Transaction
+    {
+        private int _depositAmmount;
+
+        public void addMoney()
+        {
+
+        }
+    }
+
+    public class Withdrawel : Transaction
+    {
+        private int _withdrawAmmount;
+    }
+
+    public void subtractMoney()
+    {
+
+    }
+
+    public class DeptHandler : Transaction
+    {
+        private int _dept;
+        private int _borrowAmmount;
+        private int _paymentAmmount;
+
+        public void makePayment()
+        {
+
+        }
+
+        public void BorrowMoney()
+        {
+
+        }
+    }
 }
